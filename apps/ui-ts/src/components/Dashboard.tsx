1959625f680d84e9316e32dca01b9b583bd07afc
Chen <xiaokunchen@outlook.com>
2025-11-06 21:40:50 -0500

    styles: add appropriate spacing

--- apps/ui-ts/src/components/Dashboard.tsx
+++ apps/ui-ts/src/components/Dashboard.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Timer from "./Timer"
 export default function Dashboard() {
+    const [timerTime, setTimerTime] = useState(60);
+
     return (
-    <div>
+    <div className="px-8 py-12 flex-row flex h-full font-mono">
+        <div
+          className="flex flex-col border h-full px-20 py-6"
+        >
+            <div>
+              Current Task:
+              <div className="border-2 h-20 mb-12 rounded-2xl">
+
+              </div>
+            </div>
+            <Timer onTimeChange={(t) => {setTimerTime(t)}} initialSec={timerTime} />
+        </div>
+
         <div>
-            <Timer />
-        </div>  
+          stats
+        </div>
     </div>
     );
 }
