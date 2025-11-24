1837198d165a355811bdb98558951b184c14fd17
Chen <xiaokunchen@outlook.com>
2025-11-23 21:49:04 -0500

    feat: add deleteTask and updateTask api to todoContext

--- apps/ui-ts/src/global.d.ts
+++ apps/ui-ts/src/global.d.ts
@@ -1,17 +1,30 @@
+import { TaskType } from './types';
+
 export {};
 
 declare global {
   interface Window {
     noncrast?: {
       getTasks?: () => Promise<import('./types').TaskType[]>;
+
       getMockTasks?: () => import('./types').TaskType[];
+
       createTask?: (
         input: import('./types').AddableTask
       ) => Promise<import('./types').TaskType>;
-      updateTask?: (
+
+      updateTaskStatus?: (
         id: number,
         status: 'todo' | 'in-progress' | 'done'
       ) => Promise<void> | void;
+
+      updateTask?: (
+        updatedTask: TaskType
+      ) => Promise<void>;
+
+      deleteTask?: (
+        taskId: TaskType['id']
+      ) => Promise<boolean>;
     };
   }
 }
